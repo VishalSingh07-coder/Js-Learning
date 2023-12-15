4fd96e4f627d89abd408003b1e49fb69c2d0b183
VishalSingh07-coder <84367090+VishalSingh07-coder@users.noreply.github.com>
2023-12-15 04:44:37 +0000

    new

---
+++ Basics_js1.js/strings.js
@@ -0,0 +1,12 @@
+const name="Vishal"   //stack memory
+const id=55;
+
+//new method
+// console.log(`my name is ${name} and my id is ${id}`);
+const str=new String('hello i am here') //heap memory
+
+
+let sentence="the lion runs fast"
+let word="lion"
+//condition
+console.log(`this word ${word} ${sentence.includes(word)?'is':'is not'} present in the sentence`);
---
+++ Basics_js1.js/variables_datatypes.js
@@ -0,0 +1,36 @@
+console.log("Om Ganeshaye Namah");
+
+console.log("\n");
+
+
+console.log("Vishal Singh Here");
+let first_name="Vishal"
+
+var last_name="Singh"
+console.log(first_name+last_name);
+//Conversion from one datatype to another
+
+
+//string to number
+let str='hey i am there'
+let str1='1'
+
+let num=Number(str)
+console.log(num);        //NaN
+
+let num1=Number(str1)
+console.log(num1);        //1
+
+//String or number to boolean
+let car='audi'
+let number=123
+
+// let isBoolean=Boolean(car)
+// console.log(isBoolean);         //true
+
+
+
+let isBoolean=Boolean(number)
+console.log(isBoolean);            //true
+
+//it gives false in ""(empty) and 0 value
