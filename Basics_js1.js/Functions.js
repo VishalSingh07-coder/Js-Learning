1b2ad94bf12f81fb26872a2904e5c6682e72522e
VishalSingh07-coder <84367090+VishalSingh07-coder@users.noreply.github.com>
2023-12-15 11:25:39 +0000

    functions create

---
+++ Basics_js1.js/Functions.js
@@ -0,0 +1,24 @@
+//nested functions
+
+function one(){
+    const username="Vishal"
+    function two()
+    {
+        const website=" youtube"
+        // console.log(username+website);
+    }
+
+    two()
+}
+
+one()
+
+if(true){
+    const username="Vishal Singh"
+    if(username==="Vishal Singh")
+    {
+        const website=" youtube"
+        console.log(username+website);
+    }
+
+}
